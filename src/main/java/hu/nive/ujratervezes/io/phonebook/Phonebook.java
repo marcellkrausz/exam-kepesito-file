3b19eed2d5944a16462268170aa8cd2c515c9cdf
marcellkrausz <krauszmaarcell@gmail.com>
2021-04-29 10:39:08 +0200

    add solution

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,26 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class Phonebook {
+
+    public static void exportPhonebook(Map<String, String> contacts, String output) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(output))) {
+            bw.write(mapToString(contacts));
+        } catch (IOException a) {
+            System.out.println(a.getMessage());
+        }
+    }
+
+    public static String mapToString(Map<String, String> contacts) {
+        StringBuilder sb = new StringBuilder();
+
+        for (Map.Entry<String, String> entry : contacts.entrySet()) {
+            sb.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
+        }
+        return sb.toString();
+    }
+}
